f3269f535e75998a9df42c9aa09052ab332d5406
kongshaug <benja_kongs@hotmail.com>
2019-10-07 12:57:01 +0200

    started on tests for rest

--- src/test/java/rest/PersonResourceTest.java
+++ src/test/java/rest/PersonResourceTest.java
@@ -1,11 +1,18 @@
 package rest;
 
 
+import dto.PersonDTO;
+import entities.Address;
+import entities.CityInfo;
+import entities.Hobby;
+import entities.Person;
+import entities.Phone;
 import utils.EMF_Creator;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
 import io.restassured.parsing.Parser;
 import java.net.URI;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.core.UriBuilder;
@@ -14,7 +21,12 @@ import org.glassfish.grizzly.http.util.HttpStatus;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import org.junit.After;
 import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -23,7 +35,7 @@ import utils.EMF_Creator.DbSelector;
 import utils.EMF_Creator.Strategy;
 
 //Uncomment the line below, to temporarily disable this test
-//@Disabled
+@Disabled
 public class PersonResourceTest {
 
     private static final int SERVER_PORT = 7777;
@@ -65,6 +77,33 @@ public class PersonResourceTest {
     @BeforeEach
     public void setUp() {
         
+         EntityManager em = emf.createEntityManager();
+         CityInfo cityInfo = new CityInfo(2200,"testTown");
+         Address address = new Address("streetname", "4 tv", cityInfo);
+         cityInfo.addAddress(address);
+         Phone phone = new Phone(22112211, "workPhone");
+         Hobby hoppy = new Hobby("programming", "the future of mankind is programming, also good for making a blog about your dog pictures");
+         Person p1 = new Person("jim@gmail.com", "jim", "theMan", address);
+
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Person.deleteAllRows").executeUpdate();
+            em.createNamedQuery("Address.deleteAllRows").executeUpdate();
+            em.createNamedQuery("Hobby.deleteAllRows").executeUpdate();
+            em.createNamedQuery("Phone.deleteAllRows").executeUpdate();
+            em.createNamedQuery("CityInfo.deleteAllRows").executeUpdate();
+       
+           
+            em.getTransaction().commit();
+
+        } finally {
+            em.close();
+        }
+        
+    }
+
+    @After
+    public void tearDown() throws Exception {
     }
     
     @Test
@@ -72,5 +111,153 @@ public class PersonResourceTest {
         System.out.println("Testing is server UP");
         given().when().get("/person").then().statusCode(200);
     }
+
+ 
+
+    /**
+     * Test of getPerson (with a given id) method, of class PersonResource.
+     */
+    @org.junit.Test
+    public void testGetPerson() {
+        System.out.println("getPerson");
+          given()
+                .contentType("application/json")
+                .get("/Person/1").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("hobby", hasItems("SomeHobby", 
+                        "SomeHobby"),
+                        "firstName", equalTo("name"),
+                        "lastName", equalTo("name"));
+
+    }
+    
+
+    /**
+     * Test of getPersonByPhone method, of class PersonResource.
+     */
+    @org.junit.Test
+    public void testGetPersonByPhone() {
+        System.out.println("getPersonByPhone");
+     given()
+                .contentType("application/json")
+                .get("/Person/phone/{phone}").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("hobby", hasItems("SomeHobby", 
+                        "SomeHobby"),
+                        "firstName", equalTo("name"),
+                        "lastName", equalTo("name"));
+    }
+
+    /**
+     * Test of getPersonsByHobby method, of class PersonResource.
+     */
+    @org.junit.Test
+    public void testGetPersonsByHobby() {
+        System.out.println("getPersonsByHobby");
+         given()
+                .contentType("application/json")
+                .get("/Person/persons/{hobby}").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("hobby", hasItems("SomeHobby", 
+                        "SomeHobby"),
+                        "firstName", equalTo("name"),
+                        "lastName", equalTo("name"));
+    }
+
+    /**
+     * Test of getPersonsByCity method, of class PersonResource.
+     */
+    @org.junit.Test
+    public void testGetPersonsByCity() {
+        System.out.println("getPersonsByCity");
+         given()
+                .contentType("application/json")
+                .get("/Person/city/{city}").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("hobby", hasItems("SomeHobby", 
+                        "SomeHobby"),
+                        "firstName", equalTo("name"),
+                        "lastName", equalTo("name"));
+    }
+
+    /**
+     * Test of getNumberOfPersonsWithHobby method, of class PersonResource.
+     */
+    @org.junit.Test
+    public void testGetNumberOfPersonsWithHobby() {
+        System.out.println("getNumberOfPersonsWithHobby");
+           given()
+                .contentType("application/json")
+                .get("/Person/hobby/count/{hobby}").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("hobby", equalTo("NUMBER OF PEOPLE WITH HOBBY"));
+    }
+    
+
+    /**
+     * Test of AllZipCodesInDenmark method, of class PersonResource.
+     */
+    @org.junit.Test
+    public void testAllZipCodesInDenmark() {
+        System.out.println("AllZipCodesInDenmark");
+         given()
+                .contentType("application/json")
+                .get("/Person/allzip").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("hobby", hasItems("Somezip", 
+                        "Somezip",
+                        "Somezip"));
+    }
+
+    /**
+     * Test of DeleteUser method, of class PersonResource.
+     */
+    @org.junit.Test
+    public void testDeleteUser() {
+        System.out.println("DeleteUser");
+        String id = "";
+        PersonResource instance = new PersonResource();
+        PersonDTO expResult = null;
+        PersonDTO result = instance.DeleteUser(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of EditUser method, of class PersonResource.
+     */
+    @org.junit.Test
+    public void testEditUser() {
+        System.out.println("EditUser");
+        PersonDTO person = null;
+        PersonResource instance = new PersonResource();
+        PersonDTO expResult = null;
+        PersonDTO result = instance.EditUser(person);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addPerson method, of class PersonResource.
+     */
+    @org.junit.Test
+    public void testAddPerson() {
+        System.out.println("addPerson");
+        PersonDTO person = null;
+        PersonResource instance = new PersonResource();
+        PersonDTO expResult = null;
+        PersonDTO result = instance.addPerson(person);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
    
-}
+  }
