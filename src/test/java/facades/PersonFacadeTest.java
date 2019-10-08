39eb89e884f0ec47cb70f5e6a2aa1b23f425b03d
Amalie Landt <malie@hotmail.dk>
2019-10-08 11:46:47 +0200

    personfacadetest add

--- src/test/java/facades/PersonFacadeTest.java
+++ src/test/java/facades/PersonFacadeTest.java
@@ -1,5 +1,9 @@
 package facades;
 
+import entities.Address;
+import entities.CityInfo;
+import entities.Person;
+import java.util.List;
 import utils.EMF_Creator;
 
 import javax.persistence.EntityManager;
@@ -7,6 +11,7 @@ import javax.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -56,6 +61,109 @@ public class PersonFacadeTest {
     @AfterEach
     public void tearDown() {
 //        Remove any data after each test was run
+    }
+
+    /**
+     * Test of getFacadeExample method, of class PersonFacade.
+     */
+    @org.junit.Test
+    public void testGetFacadeExample() {
+        System.out.println("getFacadeExample");
+        EntityManagerFactory _emf = null;
+        PersonFacade expResult = null;
+        PersonFacade result = PersonFacade.getFacadeExample(_emf);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPerson method, of class PersonFacade.
+     */
+    @org.junit.Test
+    public void testGetPerson() {
+        System.out.println("getPerson");
+        int number = 0;
+        PersonFacade instance = null;
+        Person expResult = null;
+        Person result = instance.getPerson(number);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPersonsByHobby method, of class PersonFacade.
+     */
+    @org.junit.Test
+    public void testGetPersonsByHobby() {
+        System.out.println("getPersonsByHobby");
+        String hobby = "";
+        PersonFacade instance = null;
+        List<Person> expResult = null;
+        List<Person> result = instance.getPersonsByHobby(hobby);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPersonsByCity method, of class PersonFacade.
+     */
+    @org.junit.Test
+    public void testGetPersonsByCity() {
+        System.out.println("getPersonsByCity");
+        CityInfo city = null;
+        PersonFacade instance = null;
+        List<Person> expResult = null;
+        List<Person> result = instance.getPersonsByCity(city);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPersonCountByHobby method, of class PersonFacade.
+     */
+    @org.junit.Test
+    public void testGetPersonCountByHobby() {
+        System.out.println("getPersonCountByHobby");
+        String hobby = "";
+        PersonFacade instance = null;
+        int expResult = 0;
+        int result = instance.getPersonCountByHobby(hobby);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getZipcodes method, of class PersonFacade.
+     */
+    @org.junit.Test
+    public void testGetZipcodes() {
+        System.out.println("getZipcodes");
+        PersonFacade instance = null;
+        List<Integer> expResult = null;
+        List<Integer> result = instance.getZipcodes();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPersonsByAdress method, of class PersonFacade.
+     */
+    @org.junit.Test
+    public void testGetPersonsByAdress() {
+        System.out.println("getPersonsByAdress");
+        Address address = null;
+        PersonFacade instance = null;
+        List<Person> expResult = null;
+        List<Person> result = instance.getPersonsByAdress(address);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
 }
